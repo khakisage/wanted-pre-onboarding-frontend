7545d8db1d551fc8ecffe4fcd542a86066040416
khakisage <jongjin201@gmail.com>
2023-01-30 10:32:16 +0900

    feat: Button Component

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,5 +1,30 @@
-import React from "react";
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-export default function Button() {
-  return <div>Button</div>;
+export default function Button({ children, onClick }) {
+  const btnStyle = css({
+    width: "100px",
+    height: "50px",
+    margin: "5px",
+    backgroundColor: "lightgrey",
+    border: "1px solid #e5e5e5",
+    borderRadius: "5px",
+    color: "black",
+    fontSize: "15px",
+    fontWeight: "bold",
+    cursor: "pointer",
+    "&:hover": {
+      backgroundColor: "grey",
+      color: "white",
+    },
+    "&:active": {
+      backgroundColor: "black",
+      color: "white",
+    },
+  });
+  return (
+    <button css={btnStyle} onClick={onClick}>
+      {children}
+    </button>
+  );
 }
