63e7435c523de0c4765680b62779a4d28ae0d1fd
khakisage <jongjin201@gmail.com>
2023-01-30 10:33:33 +0900

    feat: Header component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,20 +1,33 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
+import Button from "./Button";
+import { useNavigate } from "react-router-dom";
 export default function Header() {
+  const navigate = useNavigate();
   const header = css({
     display: "flex",
-    justifyContent: "space-between",
+    flexDirection: "row-reverse",
     alignItems: "left",
     padding: "0 20px",
     height: "60px",
     backgroundColor: "white",
     boxShadow: "0 1px 3px 0 rgba(0, 0, 0, 0.1)",
   });
+  const moveToSignup = () => {
+    navigate("/signup");
+  };
+  const moveToSignin = () => {
+    navigate("/signin");
+  };
+  const moveToTodo = () => {
+    navigate("/todo");
+  };
+
   return (
     <div css={header}>
-      <button>회원가입</button>
-      <button>로그인</button>
-      <button>todo</button>
+      <Button onClick={moveToSignup}>회원가입</Button>
+      <Button onClick={moveToSignin}>로그인</Button>
+      <Button onClick={moveToTodo}>todo</Button>
     </div>
   );
 }
